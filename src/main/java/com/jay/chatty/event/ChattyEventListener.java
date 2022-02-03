5e7c11fd00f965fe61d7f0ff4dcc363ccf485cc1
StellarisJAY <760409558@qq.com>
2022-02-03 20:16:09 +0800

    添加聊天事件监听器

--- src/main/java/com/jay/chatty/event/ChattyEventListener.java
+++ src/main/java/com/jay/chatty/event/ChattyEventListener.java
@@ -1,6 +1,12 @@
 package com.jay.chatty.event;
 
+import com.jay.chatty.mute.MuteManager;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 /**
  * <p>
@@ -12,4 +18,24 @@ import org.bukkit.event.Listener;
  */
 public class ChattyEventListener implements Listener {
 
+    private final MuteManager muteManager;
+
+    public ChattyEventListener(MuteManager muteManager) {
+        this.muteManager = muteManager;
+    }
+
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+        // 玩家加入游戏，加载玩家的屏蔽列表
+
+    }
+
+    @EventHandler(priority = EventPriority.HIGHEST)
+    public void onChatEvent(AsyncChatEvent event){
+        if(muteManager.getMuteAllSwitch()){
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(ChatColor.AQUA + "无法发送消息，服务器处于禁言状态");
+        }
+    }
 }
