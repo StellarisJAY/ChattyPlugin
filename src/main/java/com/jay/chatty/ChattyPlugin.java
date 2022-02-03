d667b8386a35a9cbe1f9a94c7b843044962a54bc
StellarisJAY <760409558@qq.com>
2022-02-03 20:17:22 +0800

    添加全服禁言命令处理

--- src/main/java/com/jay/chatty/ChattyPlugin.java
+++ src/main/java/com/jay/chatty/ChattyPlugin.java
@@ -1,5 +1,10 @@
 package com.jay.chatty;
 
+import com.jay.chatty.command.ChattyCommands;
+import com.jay.chatty.command.MuteCommandExecutor;
+import com.jay.chatty.event.ChattyEventListener;
+import com.jay.chatty.mute.MuteManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -8,13 +13,26 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public final class ChattyPlugin extends JavaPlugin {
 
+    private final MuteManager muteManager = new MuteManager();
+    private final ChattyEventListener eventListener = new ChattyEventListener(muteManager);
+    private final MuteCommandExecutor muteCommandExecutor = new MuteCommandExecutor(muteManager);
+
     @Override
     public void onEnable() {
+        //register commands
+        getCommand(ChattyCommands.MUTE_ALL).setExecutor(muteCommandExecutor);
+        getCommand(ChattyCommands.UN_MUTE_ALL).setExecutor(muteCommandExecutor);
+        getCommand(ChattyCommands.MUTE).setExecutor(muteCommandExecutor);
+        getCommand(ChattyCommands.UN_MUTE).setExecutor(muteCommandExecutor);
 
+        //register event listeners
+        Bukkit.getPluginManager().registerEvents(eventListener, this);
+
+        getLogger().info("Chatty 加载完成");
     }
 
     @Override
     public void onDisable() {
-
+        getLogger().info("Chatty 已禁用");
     }
 }
--- src/main/java/com/jay/chatty/command/MuteCommandExecutor.java
+++ src/main/java/com/jay/chatty/command/MuteCommandExecutor.java
@@ -1,9 +1,11 @@
 package com.jay.chatty.command;
 
+import com.jay.chatty.mute.MuteManager;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -15,9 +17,31 @@ import org.jetbrains.annotations.NotNull;
  * @date 2022/02/03 19:33
  */
 public class MuteCommandExecutor implements CommandExecutor {
+
+    private final MuteManager muteManager;
+
+    public MuteCommandExecutor(MuteManager muteManager) {
+        this.muteManager = muteManager;
+    }
+
     @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        String commandName = command.getName();
 
+        if(commandName.equals(ChattyCommands.MUTE_ALL)){
+            if(muteManager.setMuteAllSwitch(true)){
+                Bukkit.broadcastMessage(ChatColor.RED + "! ! ! 全服禁言开启 ! ! ! ");
+            }else{
+                sender.sendMessage("重复命令，服务器已经处于禁言状态。。。");
+            }
+        }
+        else if(commandName.equals(ChattyCommands.UN_MUTE_ALL)){
+            if(muteManager.setMuteAllSwitch(false)){
+                Bukkit.broadcastMessage(ChatColor.RED + "! ! ! 全服禁言已解除 ! ! ! ");
+            }else{
+                sender.sendMessage("无效命令，服务器位未于禁言状态。。。");
+            }
+        }
         return true;
     }
 }
