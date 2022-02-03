79daccb82bb38e0c0539fdff013b568c30174a79
StellarisJAY <760409558@qq.com>
2022-02-03 20:15:35 +0800

    添加命令常量

---
+++ src/main/java/com/jay/chatty/command/ChattyCommands.java
@@ -0,0 +1,19 @@
+package com.jay.chatty.command;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Jay
+ * @date 2022/02/03 19:50
+ */
+public class ChattyCommands {
+    public static final String MUTE_ALL = "mute-all";
+
+    public static final String MUTE = "mute";
+
+    public static final String UN_MUTE = "unmute";
+
+    public static final String UN_MUTE_ALL = "unmute-all";
+}
