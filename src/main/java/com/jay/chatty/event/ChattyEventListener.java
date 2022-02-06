5b23dea30853dc7703885e33289d58a28d83e291
StellarisJAY <760409558@qq.com>
2022-02-06 16:14:52 +0800

    添加玩家登录时禁言状态提醒

--- src/main/java/com/jay/chatty/event/ChattyEventListener.java
+++ src/main/java/com/jay/chatty/event/ChattyEventListener.java
@@ -1,12 +1,15 @@
 package com.jay.chatty.event;
 
 import com.jay.chatty.mute.MuteManager;
+import com.jay.chatty.mute.MutedPlayer;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.world.WorldSaveEvent;
 
 /**
  * <p>
@@ -27,8 +30,20 @@ public class ChattyEventListener implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
+        Player player = event.getPlayer();
+        String playerName = player.getName();
+        // 检查新加入玩家是否被禁言
+        MutedPlayer muted = muteManager.getPlayer(playerName);
+        if(muted != null){
+            // 发送提示
+            player.sendMessage(ChatColor.YELLOW + (muted.getMuteEnd() == -1 ? "您的账号处于永久禁言状态，请联系管理员解除" : "您的账号处于禁言状态，剩余时长：" + muted.getMuteTime() / 60000 + " 分钟") );
+        }
         // 玩家加入游戏，加载玩家的屏蔽列表
+    }
 
+    @EventHandler
+    public void onSave(WorldSaveEvent event){
+        // 世界保存事件
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
